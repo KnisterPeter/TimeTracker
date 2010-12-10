1d7f1fde85dfc5a932aa52cb5e45fd82343797b3
Markus Wolf <markus@emedia-solutions-wolf.de>
2010-12-10 08:37:40 +0100

    Updated to qooxdoo 1.3

--- configure.sh
+++ configure.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-[ -d "qooxdoo-1.2.1-sdk" ] && exit
-wget "http://downloads.sourceforge.net/qooxdoo/qooxdoo-1.2.1-sdk.zip" && unzip qooxdoo-1.2.1-sdk.zip
+#VERSION="1.2.1"
+VERSION="1.3"
+
+[ -d "qooxdoo-$VERSION-sdk" ] && exit
+wget "http://downloads.sourceforge.net/qooxdoo/qooxdoo-$VERSION-sdk.zip" && unzip qooxdoo-$VERSION-sdk.zip
 
